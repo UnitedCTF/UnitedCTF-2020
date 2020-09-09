d997c15bd6a44083454234bf7372fb7dda8a0d18
Francis Labelle <francis.labelle22@gmail.com>
2020-09-09 11:37:54 -0400

    Add timer to make sure the solution was scripted

--- challenges/programming/init-socket/server.py
+++ challenges/programming/init-socket/server.py
@@ -10,10 +10,15 @@ import flag
 class TaskHandler(socketserver.BaseRequestHandler):
     def main(self, client):
         name = names.get_full_name()
+        start_time = time.time()
+
         client.sendall(name.encode() + b"\n")
         answer = client.recv(1024).decode().strip()
+        end_time = time.time()
 
-        if answer == name:
+        if end_time - start_time >= 1.0:
+            client.sendall(b"Too slow! You have 1 second to send back the text that was sent to you.")
+        elif answer == name:
             client.sendall(flag.flag.encode())
         else:
             client.sendall(b"Wrong answer! You must send back the text that was sent to you.")
