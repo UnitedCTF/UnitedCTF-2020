8b0f9f68306872c30d47465c1fd6033e948fc368
CycleOfTheAbsurd <alex@barbe.red>
2020-09-10 10:33:42 -0400

    Create 3rd SQLi challenge: Error based.

--- challenges/web/sqli/sqli_web/src/includes/mysql_connection.php
+++ challenges/web/sqli/sqli_web/src/includes/mysql_connection.php
@@ -3,6 +3,7 @@ function getConnection($num_challenge) {
 	$CREDS = [
 		[$_ENV['CHALLENGE0_USER'], $_ENV['CHALLENGE0_USER_PASS']],
 		[$_ENV['CHALLENGE1_USER'], $_ENV['CHALLENGE1_USER_PASS']],
+		[$_ENV['CHALLENGE2_USER'], $_ENV['CHALLENGE2_USER_PASS']],
 	];
 	$mysqli = new mysqli($_ENV['DB_HOST'], $CREDS[$num_challenge][0], $CREDS[$num_challenge][1], $_ENV['MYSQL_DATABASE']);
 	if ($mysqli->connect_errno) {
